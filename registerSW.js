f3e92aefc6580a3b57c684468049b12e4ba8b033
NellowTCS <neeljaiswal23@gmail.com>
2025-09-04 01:19:48 +0000

    Fix

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HTMLPlayerBeta/sw.js', { scope: '/HTMLPlayerBeta/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
